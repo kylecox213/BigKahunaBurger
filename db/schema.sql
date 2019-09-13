a666e34aa13638f1cb406c4101986e209d2c6990
Kyle Cox <kylecox213@gmail.com>
2019-09-13 13:46:20 -0400

    added schema.sql; formatted index.hnadlebars

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS tastyBurgers_DB;
+
+CREATE DATABASE tastyBurgers_DB;
---
+++ server.js
@@ -0,0 +1,38 @@
+
+//------------Dependencies-----------
+
+require("dotenv").config();
+const express = require("express");
+const exphbs = require("handle-bars");
+const db = require("./models");
+
+//------------Definitions------------
+
+//---------Global Variables----------
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware for setting up URL handling and JSON parsing
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+
+// Setting up the static file directory in the public folder
+app.use(express.static("public"));
+
+// Setting up the engine for handlebars utilization
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Setting up API and HTML routes 
+require("./routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
+
+// Sync models for sequelize and initializing the server
+db.sequelize.sync().then(function () {
+    app.listen(PORT, function () {
+        console.log("We've got ears on PORT " + PORT);
+    });
+});
+
+module.exports = app;
