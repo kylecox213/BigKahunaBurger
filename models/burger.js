3fdcc4b852b1ac258b78af7001c1f53dd280ac0b
Kyle Cox <kylecox213@gmail.com>
2019-09-13 23:38:39 -0400

    burger.js was not saved prior to previous push to repo; burger.js is now included

---
+++ models/burger.js
@@ -0,0 +1,20 @@
+module.exports = function (sequelize, DataTypes) {
+    let Burger = sequelize.define("Burger", {
+        burger_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        ordered: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        }
+    });
+    Burger.associate = function (models) {
+        Burger.belongsTo(models.Customer, {
+            foreignKey: {
+                allowNull: true
+            }
+        });
+    }
+    return Burger;
+};
